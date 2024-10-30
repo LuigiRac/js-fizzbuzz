67a372144ff721dec8c0fe197a38716048c0ded0
LuigiRac <lui.rac06@gmail.com>
2024-10-30 09:09:47 +0100

    "for"

--- script/script.js
+++ script/script.js
@@ -1,4 +1,14 @@
+/* Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
+
 "use scrict"
 console.clear()
 
 const numeri = 100;
+
+for (let i = 1; i <=100; i++) {
+    if () {
+        
+    }
+}
