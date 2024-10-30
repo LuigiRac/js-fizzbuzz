41e95a2de847562343516ace89950bbf6be1a17c
LuigiRac <lui.rac06@gmail.com>
2024-10-30 09:16:48 +0100

    else n1

--- script/script.js
+++ script/script.js
@@ -8,7 +8,13 @@ console.clear()
 const numeri = 100;
 
 for (let i = 1; i <=100; i++) {
-    if () {
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    }
+
+    else if (i % 5 === 0){
+        console.log("Buzz");
         
     }
+   
 }
